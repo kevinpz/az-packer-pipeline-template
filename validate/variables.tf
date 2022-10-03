dd870ea051dc3423649fa8b7af3f86ba5610734e
Kevin Printz <kevinprintz@microsoft.com>
2022-10-03 07:06:49 -0400

    fix

---
+++ validate/variables.tf
@@ -0,0 +1,39 @@
+variable "location" {
+  type = string
+}
+
+variable "rg_name" {
+  type = string
+}
+
+variable "gallery_name" {
+  type = string
+}
+
+variable "image_name" {
+  type = string
+}
+
+variable "image_type" {
+  type = string
+}
+
+variable "rg_vnet_name" {
+  type = string
+}
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "subnet_name" {
+  type = string
+}
+
+variable "image_build_sku" {
+  type = string
+}
+
+variable "image_build_sku" {
+  type = string
+}
